22e4a54473aebb4d93ea835ef952954dd29b21f3
vanche <vanche9@gmail.com>
2016-07-25 18:03:01 +0900

    Add files via upload
    stack 및 bar 구현

--- unit.test.js
+++ unit.test.js
@@ -6,6 +6,12 @@ QUnit.module("unit.test.js", function (hooks) {
 		var fontColor = [];
 		var backgroundColor = [];
 
+
+		var Concat = function(_value){
+			value = value.concat(_value);
+		};
+
+	
 		var Push = function(_value, _fontColor, _backgroundColor){
 			value.push(_value);
 			fontColor.push(_fontColor);
@@ -13,23 +19,71 @@ QUnit.module("unit.test.js", function (hooks) {
 		};
 
 		var getValue = function(idx){
+
 			return value[idx];
 		};
 		
+	/*	var drawArray = function(section){
+			var svg = d3.select(section).append("svg");
+			var text = svg.selectAll("text._text")
+						.data(value)
+						.enter()
+						.append("text")
+						.text(function(d,i){
+							return d;
+						});
+						//.text(fuctiond(d,i){return d});
+		};
+*/
+
+		var InsertionSort = function(){
+			var temp;
+			var i;
+			var j;
+			var len = value.length;
+			
+			for(i = 1; i < len; i++){
+				temp = value[i];
+				j = i-1;
+				while((temp < value[j]) && (j>=0)){
+					value[j+1] = value[j];
+					j = j-1;
+				}
+
+				value[j+1] = temp;
+			}
+
+		}
+
+		
 		return {
 			getValue : getValue,
-			Push : Push
+			Push : Push,
+			Concat : Concat,
+			InsertionSort : InsertionSort
+		//	drawArray : drawArray
 		};
 	}
 	
 	QUnit.test( "hello test", function( assert ) {
 		var a = new ArrayModule();
 		var a2 = new ArrayModule();
-		//a.Push(1);
-		//a2.PUsh(2);
+		assert.ok("hello"===1);
+		var arr = [5,4,3,2,1];
+		a.Concat(arr);
+		assert.ok(a.getValue(4)=== 1);
+		console.log(a);
+
+		a.InsertionSort();
+		assert.ok(a.getValue(0)== 1);
+		assert.ok(a.getValue(1)== 2);
+		assert.ok(a.getValue(2)== 3);
+		assert.ok(a.getValue(3)== 4);
+		assert.ok(a.getValue(4)== 5);
+		/*
 		console.log(a);
 		console.log(a2);
-		//var a2 = new ArrayModule;
+		
 		a.Push(1,"red","white");
 		a2.Push(2,"black","blue");
 		var t = a.getValue(0);
@@ -39,8 +93,10 @@ QUnit.module("unit.test.js", function (hooks) {
 		assert.ok(a.getValue(0) === 2);
 
 		assert.ok(a2.getValue(0) === 2);
-		//assert.ok(1=="1");
-		//assert.ok(a.a ===1);
+		*/
+		//a.Push({5,4,3,2,1});
+		//assert.ok(a.getvalue[0] === 5);
+		//console.log(a);
 	});
 
 
